b817157305ad9ee057d3938913d087439a18c4f8
mordrax <joseph.ni@gmail.com>
2023-05-25 20:40:28 +1000

    init

---
+++ Player.gd
@@ -0,0 +1,33 @@
+extends Area2D
+
+@export var speed=400
+
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector. (0,0)
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1 # (1,0)
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed # (400,0)
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+			
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)	
