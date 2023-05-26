1717c17569aeccafe8d28cc0fa32565c6104a5eb
mordrax <joseph.ni@gmail.com>
2023-05-26 21:31:35 +1000

    made hud scene

--- main.gd
+++ main.gd
@@ -22,9 +22,6 @@ func new_game():
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
 
-func _on_score_timer_timeout():
-	score += 1
-
 func _on_start_timer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
@@ -53,3 +50,7 @@ func _on_mob_timer_timeout():
 
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
+
+
+func _on_score_timer_triggered():
+	score += 1
