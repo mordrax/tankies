e8a1e324ff8c46acd178797bca061d2d8bd988dd
Richard Ni <Richard@N2s-MacBook-Pro.lan>
2023-05-26 21:29:35 +1000

    add a comment

--- Player.gd
+++ Player.gd
@@ -9,7 +9,7 @@ var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
-
+	# Hello, this is a commit from Richie
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
